2b6fe34a716d20918d55731acef3e992d9ae5e42
Martin-Bonchev <mpbonchev@gmail.com>
2023-10-28 20:10:52 +0300

    Added transition to theme toggle button

--- assets/js/toggle_buttons.js
+++ assets/js/toggle_buttons.js
@@ -1,16 +1,17 @@
 "use strict"
 
-document.getElementById("theme-toggle-img").addEventListener("click", themeToggleButton);
+var themeToggleBtn = document.getElementById("theme-toggle-img");
+themeToggleBtn.addEventListener("click", toggleTheme);
 
-function themeToggleButton() {
+function toggleTheme() {
     var img_src = document.getElementById("theme-toggle-img").src;
     var img_filename = img_src.substring(img_src.lastIndexOf('/') + 1);
-
+    
     if (!(img_filename.localeCompare("sun.svg"))) { // localeCompare() returns 0 if strings are equal
         document.getElementById("theme-toggle-img").src = img_src.substring(0, img_src.lastIndexOf('/') + 1) + "moon.svg";
-        document.getElementById("theme-toggle-img").style.marginLeft = "auto";
+        themeToggleBtn.classList.add("item-self-right");
     } else {
         document.getElementById("theme-toggle-img").src = img_src.substring(0, img_src.lastIndexOf('/') + 1) + "sun.svg";
-        document.getElementById("theme-toggle-img").style.marginLeft = "0";
+        themeToggleBtn.classList.toggle("item-self-right");
     }
 }
