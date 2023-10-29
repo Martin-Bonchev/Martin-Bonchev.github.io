0f4c3012788ee41736d11234d078b6238516a1c4
Martin-Bonchev <mpbonchev@gmail.com>
2023-10-29 16:33:27 +0200

    Added background images and changes to toggle button
    Added background images for light and dark theme;
    Changed moon.svg image;
    Changed toggle button background colors;

--- assets/js/toggle_buttons.js
+++ assets/js/toggle_buttons.js
@@ -8,10 +8,16 @@ function toggleTheme() {
     var img_filename = img_src.substring(img_src.lastIndexOf('/') + 1);
     
     if (!(img_filename.localeCompare("sun.svg"))) { // localeCompare() returns 0 if strings are equal
-        document.getElementById("theme-toggle-img").src = img_src.substring(0, img_src.lastIndexOf('/') + 1) + "moon.svg";
-        themeToggleBtn.classList.add("item-self-right");
+        document.getElementById("theme-toggle-img").src = img_src.substring(0, img_src.lastIndexOf('/') + 1) + "moon.svg"; // Change toggle button image
+        themeToggleBtn.classList.replace("item-self-left", "item-self-right");
+        themeToggleBtn.classList.replace("bg-white", "bg-light-grey");
+        document.getElementById("theme-toggle-box").classList.replace("bg-light-blue", "bg-dark-blue");
+        document.body.classList.replace("bg-light-theme", "bg-dark-theme"); // Change background image
     } else {
         document.getElementById("theme-toggle-img").src = img_src.substring(0, img_src.lastIndexOf('/') + 1) + "sun.svg";
-        themeToggleBtn.classList.toggle("item-self-right");
+        themeToggleBtn.classList.replace("item-self-right", "item-self-left");
+        themeToggleBtn.classList.replace("bg-light-grey", "bg-white");
+        document.getElementById("theme-toggle-box").classList.replace("bg-dark-blue", "bg-light-blue");
+        document.body.classList.replace("bg-dark-theme", "bg-light-theme"); 
     }
 }
