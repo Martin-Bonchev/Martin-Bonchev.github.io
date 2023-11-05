75ec60ed2f2102a7ab040ea5a025572b421460ac
Martin-Bonchev <mpbonchev@gmail.com>
2023-11-06 00:09:20 +0200

    Styled email form

--- assets/js/send_mail.js
+++ assets/js/send_mail.js
@@ -2,7 +2,7 @@ function sendMail() {
     const a =`mailto:mpbonchev@gmail.com
         ?cc=${document.getElementById("emailCC").value}
         &subject=${document.getElementById("emailSubject").value}
-        &body=${document.getElementById("emailName").value},%0D%0A${document.getElementById("emailMessage").value.split("\n").join("%0D%0A")}
+        &body=${document.getElementById("emailName").value},%0D%0A%0D%0A${document.getElementById("emailMessage").value.split("\n").join("%0D%0A")}
     `;
     window.open(a);
 }
